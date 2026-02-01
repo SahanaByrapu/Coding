2b0d1714451a67a743d3419d09c895c86e93eb7e
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-01-31 16:49:38 -0800

    Implement containsDuplicate function in Python
    Added Java and Python implementation for containsDuplicate function.
    
    Time Complexity O(n)

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 1 - Basics/1. Contains Duplicate/1.Contains Duplicate.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 1 - Basics/1. Contains Duplicate/1.Contains Duplicate.py
@@ -1,1 +1,34 @@
 """ https://leetcode.com/problems/contains-duplicate/description/ """
+
+### java
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> freqset =new HashSet<>();
+
+        for(int num:nums)
+        {
+            if(freqset.contains(num))
+             return true;
+            
+            freqset.add(num);
+        }
+
+        return false;
+    }
+}
+
+
+### Python
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+       visited=set()
+
+       for num in nums:
+         if num in visited:
+             return True
+         visited.add(num)
+
+       return False
