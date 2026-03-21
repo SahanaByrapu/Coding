c532cafcec6d054efd99f0490d92079c5d66f453
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-21 16:30:49 -0700

    Add maxSubArray function for maximum subarray sum
    Implemented the maxSubArray function to find the maximum sum of contiguous subarray.

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 3- Frequency & Majority/Maximum Subarray/Maximum Subarray.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 3- Frequency & Majority/Maximum Subarray/Maximum Subarray.py
@@ -1,1 +1,17 @@
 """https://leetcode.com/problems/maximum-subarray/description/"""
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        subsum=nums[0]
+        maxsum=nums[0]
+
+        for i in range(1,len(nums)):
+             subsum=max(nums[i],subsum+nums[i])
+             maxsum=max(maxsum,subsum)
+
+
+        return maxsum
+
+
+#TimeComplexity : O(n)
+#SpaceComplexity : O(1)
