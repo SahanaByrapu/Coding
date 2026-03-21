1eb0f2bcab75be5869dc575cb7befc5d7151a810
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-21 15:21:18 -0700

    Add majorityElement function using Boyer-Moore algorithm
    Implement Boyer-Moore Voting Algorithm to find the majority element in a list.

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 3- Frequency & Majority/Majority Element/Majority Element.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 3- Frequency & Majority/Majority Element/Majority Element.py
@@ -1,1 +1,19 @@
 """https://leetcode.com/problems/majority-element/description/"""
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+     count=0
+     for i in range(0,len(nums)):
+     #Boyer-Moore Voting Algorithm
+        if(count==0):
+            candidate=nums[i]
+            count+=1
+        else:
+          count+= 1 if(candidate==nums[i]) else -1
+
+     return candidate
+      
+
+
+#Space Complexity:O(1)
+#Time Complexity:O(n)
