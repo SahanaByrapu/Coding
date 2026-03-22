710d989ebc3a1bbddc94b41c733a7ce4adf91d69
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-22 11:49:31 -0700

    Implement longest subarray with sum K function
    Fix variable name typo and add function definition for longest subarray with sum K.

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 4 - Subarray with Given Sum/ Longest Subarray With Sum K -GFG/ Longest Subarray With Sum K.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 4 - Subarray with Given Sum/ Longest Subarray With Sum K -GFG/ Longest Subarray With Sum K.py
@@ -1,1 +1,19 @@
 """https://www.geeksforgeeks.org/dsa/longest-sub-array-sum-k/"""
+
+def longestSubarrayWithSumK(arr,k):
+
+  hmap={}
+  prefixsum=0
+  maxlength=0
+  for i in range(len(arr)):
+      prefixsum=prefixsum+arr[i]
+      if prefixsum not in hmp:
+        hmp[prefixsum]=i
+      elif prefixsum==k:
+        maxlen=max(maxlen, i+1)
+        
+  return maxlen
+
+#TimeComplexity: O(n)
+#SpaceComplexity: O(1)
+  
