f63e190189520fde60adfe5ad0d835b07c6204b0
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-22 13:12:58 -0700

    Refactor longestSubarrayWithEqualZerosandOnes function
    Fix indentation and update logic for finding the longest subarray with equal number of 0s and 1s.

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 4 - Subarray with Given Sum/Longest Subarray With Equal Number of 0s and 1s -GFG/Longest Subarray With Equal Number of 0s and 1s.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 4 - Subarray with Given Sum/Longest Subarray With Equal Number of 0s and 1s -GFG/Longest Subarray With Equal Number of 0s and 1s.py
@@ -1,1 +1,32 @@
 """https://www.geeksforgeeks.org/dsa/largest-subarray-with-equal-number-of-0s-and-1s/"""
+
+def longestSubarrayWithEqualZerosandOnes(arr):
+
+  hmap={}
+  prefixsum=0
+  maxlen=0
+
+   
+    for i in range(0,len(arr)):
+        # if current element is zero, add -1
+        prefixsum+=-1 if arr[i]==0 else 1
+
+         # To handle sum = 0 at last index
+        if prefixsum==0:
+       		maxlen=i+1
+
+        # If this sum is seen before, then update 
+      	# result with maximum
+        if prefixsum in hmap:
+          maxlen=max(maxlen, i-arr[prefixsum])
+        # Else put this sum in hash table
+        else:
+          hmap[prefixsum]=i
+          
+    return maxlen
+
+if __name__ == "__main__":
+    arr = [0]
+    print(maxLen(arr))
+
+    
