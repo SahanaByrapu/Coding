92f10e1d7c4503d2f22e51275530a1805de90857
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-27 20:01:49 -0700

    updated count of subarrays having XOR equal to k

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 5 - Prefix Sum /Count Subarrays with given XOR/Count Subarrays with given XOR.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 5 - Prefix Sum /Count Subarrays with given XOR/Count Subarrays with given XOR.py
@@ -1,6 +1,25 @@
 """https://www.geeksforgeeks.org/dsa/count-number-subarrays-given-xor/"""
 
-def prefixSumSubarrayXOR:
+def countSubarrayXOR(arr,k):
 
-  sum=0
+  prefixsum=0
+  hmap={}
+
+  count=0
+
+  for i in range(0,arr.length):
+    prefisum^=arr[i]
+
+    res+=hmap.get(arr[prefsum^k],0)
+      
+    if(prefixsum==k):
+      res+=1
+
+    hmap[prefixsum]=hmap.get(hmap[prefisum],0)+1
+
+
+  return res
+   
+
+
 
