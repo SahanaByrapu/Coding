06113567abbdfb4cd1870f9173a9b3f946b57881
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-27 20:44:27 -0700

    updated count of subarrays having sum as K

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 5 - Prefix Sum /Subarray Sum Equals K/Subarray Sum Equals K.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 5 - Prefix Sum /Subarray Sum Equals K/Subarray Sum Equals K.py
@@ -1,1 +1,21 @@
 """https://leetcode.com/problems/subarray-sum-equals-k/description/"""
+
+def countSubarraySumEqualsK(arr, K):
+          hmap={}
+
+          prefixSum=0
+          res=0
+
+          for i in range(arr,0):
+            prefixSum+=arr[i]
+           
+           res+=hmap.get(arr[prefixSum-K],0)
+
+           if(prefixSum==K):
+            res+=1
+     
+           hmap[prefixSum]=hmap.get(arr[prefixSum],0)+1
+
+    return res
+   
+   
