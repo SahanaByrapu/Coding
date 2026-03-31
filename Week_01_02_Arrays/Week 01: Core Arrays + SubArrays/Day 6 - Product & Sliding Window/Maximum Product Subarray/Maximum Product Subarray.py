249665787a0c67107ed0ab2c94f78ff547d20116
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-03-30 21:38:56 -0700

    Update Maximum Product Subarray.py

--- Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 6 - Product & Sliding Window/Maximum Product Subarray/Maximum Product Subarray.py
+++ Week_01_02_Arrays/Week 01: Core Arrays + SubArrays/Day 6 - Product & Sliding Window/Maximum Product Subarray/Maximum Product Subarray.py
@@ -1,12 +1,21 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
 
-        res=1
-        prefixProd=1
+        rres=0
+        maxsofar=nums[0]
+        minsofar=nums[0]
 
-        hmap={}
-
-        for i in range(0,nums.length):
-           prefixProd=max(prefixProd,prefixProd*nums[i])
+        for i in range(1,len(nums)):
            
+           maxsofar=max(maxsofar,maxsofar*nums[i])
+           minsofar=min(minsofar, minsofar*nums[i])
 
+           int tmp_max=Math.max(curr,Math.max(curr*max_so_far,curr*min_so_far));
+           min_so_far=Math.min(curr,Math.min(curr*max_so_far,curr*min_so_far));
+
+           max_so_far=tmp_max;
+           res=Math.max(res,max_so_far);
+
+           res= max(res, maxsofar)
+
+        return res
